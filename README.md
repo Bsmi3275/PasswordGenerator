9b8cddee5452c6e1545f7eaf316a463a2e72fc53
Benjamin Smith <70050256+Bsmi3275@users.noreply.github.com>
2020-11-23 12:52:57 -0500

    Add files via upload
    completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # PasswordGenerator
+
+This application has been (on and off) a long time in the making.
+
+
+
+The most frustrating thing about this homework assignment is the simplicity of what a coder can miss and the carefullness that is needed to code effectively (without anything going wrong).
+
+Things started going in the right direction once I began focusing on the instructions given rather than improving the css or finding the css link.
+
+I had to separate out the lowercase and uppercase variables and include the separate "special" and "numeric" characters in the javascript. 
+
+- For the for loop, I did "length -1", because the loop begins at 0 rather than 1. 
+- I also had to add "newPassword" in its own equation. 
+
+Using my assignments, the weather dashboard and the first project, I tried to do an ajax call under the foor loop. Of course, this did not work. 
+
+So I just declared "newPassword" and directly set it equal to the random math equation.
+
+That didn't work...
+
+
+So, I tried to append, then prepend. I went back and pre-declared "newPassword" (let newPassword = "", setting it to what it might be if a stupid user selects "cancel" on all four criteria). 
+
+I still am confused about how this works, because I again tried ajax methods but they all did not work until I declared "newPassword" and set it to the random math equation. The thing that confuses me the most is how [newPassword = newPassword + etc.] because that is like saying that 0 = 0 + 1.
+
+Or is it like saying 1 = 1 + 0?...
--- script.js
+++ script.js
@@ -1,40 +1,81 @@
-functon generate(){
+//This happens with the "generate" button
+var generateBtn = document.querySelector("#generate");
 
-    let complexity = document.getElementById("slider").value;
+//creating and generating the password in one function
+function writePassword(){
 
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-
-    let password= "";
-
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length -1)));
+//alerts(validations) will be made if the criteria are positively selected by the user, but will not if they are not selected by the user. 
+    var upperCase = confirm("Do you want your password to contain uppercase characters? (Please click 'OK' for 'Yes' and 'Cancel' for 'No')");
+    if (upperCase === true){
+        var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        alert ("You will have uppercase characters is your password!")
     }
 
-    document.getElementById("display").value = password;
-
-    document.getElementById("lastNums").innerHTML += password + "<br />";
-
-}
-
-document.getElementById("length").innerHTML = "Length: 25";
-
-document.getElementById("slider").onInput = function(){
-
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;   
-    }
-    else{
-        document.getElementById("length"). innerHTML = "Length: 1";
+     else {
+        var upperCaseChars = "";
     }
 
-}
+    var lowerCase = confirm("Do you want your password to contain lowercase characters? (Please click 'OK' for 'Yes' and 'Cancel' for 'No')");
+    if (lowerCase === true){
+        var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+        alert ("You will have lowercase characters is your password!")
+    }
 
-function copyPassword(){
+    else {
+        var lowerCaseChars = "";
+    }
 
-    document.getElementById("display").select();
 
-    document.execCommand("Copy");
+    var numeric = confirm("Do you want your password to contain numeric characters? (Please click 'OK' for 'Yes' and 'Cancel' for 'No')");
+    if (numeric === true){
+        var numericChars = "1234567890";
+        alert ("You will have numeric characters is your password!")
+    }
 
-    alert("PWD copied to clipboard!");
+    else {
+        var numericChars = "";
+    }
 
-}
+
+    var special = confirm("Do you want your password to contain any special characters (*,&,!,%)? (Please click 'OK' for 'Yes' and 'Cancel' for 'No')");
+    if (special === true){
+        var specialChars = "!@#$%^&*?><";
+        alert ("You will have special characters is your password!")
+    }
+
+    else {
+        var specialChars = "";
+    }
+ 
+    //The limits outlined in the homework were less than 8 and greater than 128. 8 is min and 128 is max.
+    //I was tempted to shorten this, as no one I know has a strong password with characters over 30.
+    var length = prompt("How long (in number of characters between 8-128) do you want your password to be?");
+
+          if (length < 8){
+            alert("The password you selected is too short. You need 8 characters or more!");
+            return;
+          }
+          if (length > 128){
+            alert("The password you selected is WAY too long. You need 128 characters or less!");
+            return;
+          }
+
+    var chars = upperCaseChars + lowerCaseChars + numericChars + specialChars;
+    
+    //password is nothing prior to being generated
+    let newPassword = "";
+
+    //For loop setting the length equal to a random length
+    for (var i=0; i<= length -1; i++) {
+        //nothing + the random equation = that nothing in it of itself
+        newPassword = newPassword + chars.charAt(Math.floor(Math.random() * Math.floor(chars.length - 1)))};
+
+        //innerHTML is to transfer something from the creator's script, as "$" and "response" are for API's.
+        password.innerHTML = newPassword;
+
+    }
+
+//Click the "generate" button to see the magic done by the previous function 
+generateBtn.addEventListener("click", writePassword);
+
+
